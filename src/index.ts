0d02ac5ddbb2424b13922b34493504ad0441d13f
Harry G Withers <harrygwithers@gmail.com>
2021-11-17 16:04:54 +0000

    feat: basic views

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-import express from 'express'
 import dotenv from 'dotenv'
+import express from 'express'
+import path from 'path'
 
 dotenv.config()
 
@@ -7,8 +8,12 @@ const { PORT = 3000 } = process.env
 
 const app = express()
 
-app.get('/', (_, res) => {
-    res.send('Hello world!')
+app.get('/client', (_, res) => {
+    res.sendFile(path.resolve(__dirname, './client.html'))
+})
+
+app.get('/streamer', (_, res) => {
+    res.sendFile(path.resolve(__dirname, './streamer.html'))
 })
 
 app.listen(PORT, () => {
