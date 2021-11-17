de8dba3253b36eeda262dc401ed4436ce4abd63d
Harry G Withers <harrygwithers@gmail.com>
2021-11-17 15:52:27 +0000

    feat: express hello world

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('Hello world!')
+import express from 'express'
+
+const { PORT = 3000 } = process.env
+
+const app = express()
+
+app.get('/', (_, res) => {
+    res.send('Hello world!')
+})
+
+// start the Express server
+app.listen(PORT, () => {
+    console.log(`Server started at http://localhost:${PORT}`)
+})
